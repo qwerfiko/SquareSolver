40fb15e6ef5bcdaacba87c10466b9bb6ec085dc4
qwerfiko <142624907+qwerfiko@users.noreply.github.com>
2023-08-19 00:10:56 +0400

    Сделал квадратку!
    Не понял, как проверить, что введено именно число

---
+++ quadrate.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <TXlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+int main(){
+float a;
+float b;
+float c;
+float discr;
+float var;
+printf("Для уравнения вида ax^2+bx+c = 0 введите коэффициенты\n");
+printf("b = ");
+scanf ("%f", &b);
+printf("c = ");
+scanf ("%f", &c);
+discr = pow (b, 2) - 4*a*c;
+if (a == 0 && b!=0){
+printf("Уравнение не квадратное, а линейное. x = %f", c/b);
+}
+if (a == 0 && b ==0 && c != 0){
+printf("Корней нет. Неправильное уравнение");
+}
+if (a == 0 && b ==0 && c == 0){
+printf("Бесконечно много корней");
+}
+if (a!=0 && discr > 0){
+printf("Уравнение имеет два вещественных корня:\n");
+printf("x1 = %f\n", (-b + sqrt(discr))/2*a);
+printf("x2 = %f\n", (-b - sqrt(discr))/2*a);
+}
+if (a!=0 && discr == 0){
+printf("Уравнение имеет один вещественный корень:\n");
+printf("x = %f\n", -b/2*a);
+}
+if (a!=0 && discr < 0){
+printf("Уравнение не имеет вещественных корней\n");
+}
+return 0;
+}
