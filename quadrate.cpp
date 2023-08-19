3e795e133cab81d9853bdf21fe96917d1a2bb1ed
qwerfiko <142624907+qwerfiko@users.noreply.github.com>
2023-08-19 18:53:16 +0400

    Пофиксил != через сравнение, инициализация, отступ

--- quadrate.cpp
+++ quadrate.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <TXlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 #include <math.h>
 
-int var;
 float check_number (float x);
 void discr_on (float a, float b, float c, float discr);
 void discr_off (float a, float b, float c);
+
 int main(){
-    float a, b, c;
+    float a = 0, b = 0, c = 0;
     printf("Для уравнения вида ax^2+bx+c = 0 введите коэффициенты\n");
     printf("a = ");
     a = check_number (a);   //Проверка на ввод только числа
@@ -24,6 +25,7 @@ return 0;
 }
 
 float check_number (float x) {
+    int var;
     do {
         var = scanf("%f", &x);
     } while (getchar () != '\n');
@@ -31,25 +33,27 @@ float check_number (float x) {
 }
 
 void discr_on (float a, float b, float c, float discr) {
-    if (a!= 0 && discr > 0){
+    float epsillon = 1e-6;
+    if (abs(a - 0) > epsillon && discr>0){
         printf("Уравнение имеет два вещественных корня:\n");
         printf("x1 = %f\n", (-b + sqrt(discr))/2*a);
         printf("x2 = %f\n", (-b - sqrt(discr))/2*a);
     }
-    if (a!= 0 && discr == 0){
+    if (abs(a - 0) > epsillon && discr == 0){
         printf("Уравнение имеет один вещественный корень:\n");
         printf("x = %f\n", -b/2*a);
     }
-    if (a!= 0 && discr < 0){
+    if (abs(a - 0) > epsillon && discr < 0){
         printf("Уравнение не имеет вещественных корней\n");
     }
 }
 
 void discr_off (float a, float b, float c){
-    if (a == 0 && b!= 0){
+    float epsillon = 1e-6;
+    if (a == 0 && abs(b - 0) > epsillon){
         printf("Уравнение не квадратное, а линейное. x = %f", c/b);
     }
-    if (a == 0 && b == 0 && c != 0){
+    if (a == 0 && b == 0 && abs(c - 0) > epsillon){
         printf("Корней нет. Неправильное уравнение");
     }
     if (a == 0 && b == 0 && c == 0){
