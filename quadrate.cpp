51fe970578cdc59e70dd21d392d6d6e7a77a4f6d
qwerfiko <142624907+qwerfiko@users.noreply.github.com>
2023-08-20 23:32:09 +0400

    Переделал проверку ввода на число_2

--- quadrate.cpp
+++ quadrate.cpp
@@ -1,38 +1,46 @@
+//quadrate.c - a program for solving a quadratic equation
 #include <stdio.h>
 #include <stdlib.h>
 #include <TXlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 #include <math.h>
 
-float check_number (float x);
-void discr_on (float a, float b, float c, float discr);
-void discr_off (float a, float b, float c);
+float check_input_number (float x);
+void solve_quadratic_equation (float a, float b, float c, float discr);
+void solve_linear_equation (float a, float b, float c);
 
 int main(){
     float a = 0, b = 0, c = 0;
-    printf("Для уравнения вида ax^2+bx+c = 0 введите коэффициенты\n");
+    printf("Для уравнения вида ax^2+bx+c = 0 введите коэффициенты:\n");
     printf("a = ");
-    a = check_number (a);   //Проверка на ввод только числа
+    a = check_input_number (a);
     printf("b = ");
-    b = check_number (b);   //Проверка на ввод только числа
+    b = check_input_number (b);
     printf("c = ");
-    c = check_number (c);   //Проверка на ввод только числа
-    discr_off (a,b,c);  //Обработка случаев при a = 0
+    c = check_input_number (c);
+    if (a == 0) {    //Обработка случаев при a = 0
+    solve_linear_equation (a,b,c);
+    }
+    else {
     float discr;
     discr = pow (b, 2) - 4*a*c; //Вычисление дискриминанта
-    discr_on (a, b, c, discr);  //Обработка решений через дискриминант
+    solve_quadratic_equation (a, b, c, discr);  //Обработка решений через дискриминант
+    }
 return 0;
 }
 
-float check_number (float x) {
-    int var;
-    do {
-        var = scanf("%f", &x);
-    } while (getchar () != '\n');
+float check_input_number (float x) {
+    char ch;
+    while (scanf("%f", &x) != 1){
+    while ((ch = getchar()) != '\n')
+       putchar(ch);
+       printf(" - не могу прочитать число. Повторите попытку. \n");
+    }
     return x;
+    fflush(stdin);
 }
 
-void discr_on (float a, float b, float c, float discr) {
+void solve_quadratic_equation (float a, float b, float c, float discr) {
     float epsillon = 1e-6;
     if (abs(a - 0) > epsillon && discr>0){
         printf("Уравнение имеет два вещественных корня:\n");
@@ -48,15 +56,15 @@ void discr_on (float a, float b, float c, float discr) {
     }
 }
 
-void discr_off (float a, float b, float c){
+void solve_linear_equation (float a, float b, float c){
     float epsillon = 1e-6;
-    if (a == 0 && abs(b - 0) > epsillon){
+    if (abs(b - 0) > epsillon){
         printf("Уравнение не квадратное, а линейное. x = %f", c/b);
     }
-    if (a == 0 && b == 0 && abs(c - 0) > epsillon){
+    if (b == 0 && abs(c - 0) > epsillon){
         printf("Корней нет. Неправильное уравнение");
     }
-    if (a == 0 && b == 0 && c == 0){
+    if (b == 0 && c == 0){
         printf("Бесконечно много корней");
     }
 }
